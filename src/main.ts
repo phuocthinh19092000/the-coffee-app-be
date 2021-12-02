09c4525b7f61592895abfc2298223edf9399aca2
chinh.duongv <chinh.duongv@one-line.com>
2021-12-02 09:07:47 +0700

    feature: add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The Coffee App OTSV')
+    .setDescription('The Coffee App OTSV API Documentation')
+    .setVersion('3.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
