5ef1ba8a99aa9d07b337bd20f12eb4e7cc2d4934
chinh.duongv <chinh.duongv@one-line.com>
2021-12-03 16:08:57 +0700

    feature: api get all category

--- src/modules/categories/controllers/categories.controller.ts
+++ src/modules/categories/controllers/categories.controller.ts
@@ -1,46 +1,52 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
-import { CategoriesService } from '../services/categories.service'
+import { CategoriesService } from '../services/categories.service';
 import { Category } from '../entities/category.entity';
 import { CreateCategoryDto } from '../dto/request/create-category.dto';
 import { UpdateCategoryDto } from '../dto/request/update-category.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 @ApiTags('categories')
 @Controller('categories')
 export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
 
-    constructor(private readonly categoriesService: CategoriesService) { }
+  @Get()
+  @ApiOkResponse({
+    description: 'Get All Categories successfully.',
+    type: Category,
+  })
+  findAll(): Promise<Category[]> {
+    return this.categoriesService.findAll();
+  }
 
-    @Get()
-    @ApiOkResponse(
-        {
-            description: 'Get All Categories successfully.',
-            type: Category
-        }
-    )
-    findAll(): Promise<Category[]> {
-        return this.categoriesService.findAll();
-    }
+  @Post()
+  @ApiCreatedResponse({
+    description: 'Create Category successfully.',
+    type: Category,
+  })
+  create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return this.categoriesService.create(createCategoryDto);
+  }
 
-    @Post()
-    @ApiCreatedResponse(
-        {
-            description: 'Create Category successfully.',
-            type: Category
-        }
-    )
-    create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
-        return this.categoriesService.create(createCategoryDto);
-    }
-
-    @Patch(':id')
-    @ApiOkResponse(
-        {
-            description: 'Update Category successfully.',
-            type: Category
-        }
-    )
-    @ApiParam({ name: 'id', required: true, description: 'id of category', schema: { type: 'string' } })
-    update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto): Promise<Category> {
-        return this.categoriesService.update(id, updateCategoryDto);
-    }
+  @Patch(':id')
+  @ApiOkResponse({
+    description: 'Update Category successfully.',
+    type: Category,
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'id of category',
+    schema: { type: 'string' },
+  })
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    return this.categoriesService.update(id, updateCategoryDto);
+  }
 }
