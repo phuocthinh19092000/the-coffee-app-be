0ebfd019883a88d3ee6d88a9fe26032d4cb03f04
nguyen-dangn-otsv <nguyen.dangn@one-line.com>
2021-12-14 10:37:00 +0700

    feat: create api get information of user

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from 'src/decorators/user.decorator';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/profile')
+  getProfile(@User() user) {
+    const { password, ...restUser } = user._doc;
+    return restUser;
+  }
+}
