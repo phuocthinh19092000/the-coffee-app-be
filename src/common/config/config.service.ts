6e737259e39904131b52714e1d8125841f5bf0fc
Nguyen Dang N <nguyen.dangn@one-line.com>
2021-12-16 22:13:39 +0700

    feat: config connect server firebase

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -14,4 +14,19 @@ export class AppConfigService {
   jwtRefreshExpiration: number = this.configService.get(
     'JWT_REFRESH_EXPIRATION',
   );
+
+  // FIREBASE
+  type: string = this.configService.get<string>('type');
+  projectId: string = this.configService.get<string>('project_id');
+  privateKeyId: string = this.configService.get<string>('private_key_id');
+  privateKey: string = this.configService.get<string>('private_key');
+  clientEmail: string = this.configService.get<string>('client_email');
+  clientId: string = this.configService.get<string>('client_id');
+  authUri: string = this.configService.get<string>('auth_uri');
+  tokenUri: string = this.configService.get<string>('token_uri');
+  authProvider: string = this.configService.get<string>(
+    'auth_provider_x509_cert_url',
+  );
+  clientX509: string = this.configService.get<string>('client_x509_cert_url');
+  firebaseUrl: string = this.configService.get<string>('firebase_url');
 }
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin/app';
+import { AppConfigService } from 'src/common/config/config.service';
+import { getMessaging, getToken } from 'firebase/messaging';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+  const appConfigService = app.get(AppConfigService);
+
+  const configSwaggerDocument = new DocumentBuilder()
     .setTitle('The Coffee App OTSV')
     .setDescription('The Coffee App OTSV API Documentation')
     .setVersion('3.0')
     .addBearerAuth()
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, configSwaggerDocument);
   SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(
@@ -25,6 +29,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  const serviceFirebase: ServiceAccount = {
+    projectId: appConfigService.projectId,
+    privateKey: appConfigService.privateKey,
+    clientEmail: appConfigService.clientEmail,
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceFirebase),
+    databaseURL: appConfigService.firebaseUrl,
+  });
 
   app.enableCors();
 
